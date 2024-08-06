1b431e6249e03a4ecb77ac7193d4204cf61d41ee
chenet1220 <chrischenet1@gmail.com>
2024-08-06 16:25:25 -0400

    Blackjack

--- js/main.js
+++ js/main.js
@@ -31,10 +31,66 @@
   standButton.addEventListener('click', Stand);
 
   /*----- functions -----*/
-  initialize()
+  init();
 
+  function init() {
+    dealInitCards();
+    let playerHand = [];
+    let dealerhand = [];
+    let playerScore = 0,
+    let dealerScore = 0, 
+    render();
+  }
 
+  
   function render() {
-    renderBoard();
-    renderScores();
+
+  }
+
+  
+
+
+    function getNewShuffledDeck() {
+    // Create a copy of the originalDeck (leave originalDeck untouched!)
+    const tempDeck = [...originalDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+      // Get a random index for a card still in the tempDeck
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+  }
+
+
+  function buildOriginalDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+        deck.push({
+          // The 'face' property maps to the library's CSS classes for cards
+          face: `${suit}${rank}`,
+          // Setting the 'value' property for game of blackjack, not war
+          value: Number(rank) || (rank === 'A' ? 11 : 10)
+        });
+      });
+    });
+    return deck;
+  }
+
+
+  function renderDeckInContainer(deck, container) {
+    container.innerHTML = '';
+    // Let's build the cards as a string of HTML
+    let cardsHtml = '';
+    deck.forEach(function(card) {
+      cardsHtml += `<div class="card ${card.face}"></div>`;
+    });
+    // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+    // const cardsHtml = deck.reduce(function(html, card) {
+    //   return html + `<div class="card ${card.face}"></div>`;
+    // }, '');
+    container.innerHTML = cardsHtml;
   }
